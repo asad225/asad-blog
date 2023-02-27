0c90bc6ff782a4b29e571823faa759ce43d82925
asad225 <asadrahman225@gmail.com>
2023-02-27 12:11:17 +0500

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from './routes/user-routes'
 import cors from "cors";
-// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// dotenv.config()
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 
 const app = express();
